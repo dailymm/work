815a8a5ad7d075bc8e6f2a7dc7ca564f60c4c6ff
wenxiaomei <1349978248@qq.com>
2019-12-09 21:03:44 +0800

    环境配置

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -12,17 +12,21 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
-            'id' => $this->primaryKey(),
-            'username' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
+        $this->createTable('{{%student}}', [
+            'id'                   => $this->primaryKey(),
+            'username'             => $this->string()->notNull()->unique(),
+            'auth_key'             => $this->string(32)->notNull(),
+            'password_hash'        => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
+            'email'                => $this->string()->notNull()->unique(),
+            'status'               => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at'           => $this->integer()->unsigned()->notNull(),
+            'updated_at'           => $this->integer()->unsigned()->notNull(),
+            'student_id'           => $this->string(16)->notNull()->defaultValue('')->comment('学号'),
+            'sex'                  => $this->string('4')->defaultValue('male')->comment('性别，男：male,女：female'),
+            'depart'               => $this->string('16')->defaultValue('')->comment('系别'),
+            'class'                => $this->string(60)->defaultValue('')->comment('班别'),
 
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
     }
 
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -29,20 +29,20 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => '教师作业管理系统',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => '首页', 'url' => ['/site/index']],
+        ['label' => '上传作业', 'url' => ['/site/about']],
+        ['label' => '个人中心', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
